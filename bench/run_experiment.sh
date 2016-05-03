3d3acc8667cf8d1a9f13520de4afd50a4fdbd16c
Damian Eads <damian@wise.io>
2016-05-03 13:43:37 -0700

    Minor tweak to call.

--- bench/run_experiment.sh
+++ bench/run_experiment.sh
@@ -4,16 +4,18 @@ json_file=$1
 
 if [ "$(grep warm $json_file | wc -l)" == "1" ]
 then
-    echo warm
-    ./run_experiment.py $json_file nolog
+    echo warm: $json_file
+    cat $json_file
+    python ./run_experiment.py $json_file nolog
     for trials in $(seq 1 1); do
-        ./run_experiment.py $json_file
+        python ./run_experiment.py $json_file
     done
 else
+    echo cold: $json_file
+    cat $json_file
     for trials in $(seq 1 1); do
         free && sync && echo 3 > /proc/sys/vm/drop_caches && free
         sleep 1
-        ./run_experiment.py $json_file
+        python ./run_experiment.py $json_file
     done
-    echo cold
 fi
