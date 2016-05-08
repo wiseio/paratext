04c1491ddbe1354200069e30ff1bb244a79c5215
Damian Eads <damian@wise.io>
2016-05-08 15:10:01 -0700

    properly convert R time objects

--- bench/bench.R
+++ bench/bench.R
@@ -18,14 +18,14 @@ result_filename <- args[2]
 load_tic <- Sys.time()
 df = read.csv(filename)
 load_toc <- Sys.time()
-load_time <- load_toc - load_tic
+load_time <- as.double(difftime(load_toc, load_tic))
 
 mem <- memory_usage()
 
 sum_tic <- Sys.time()
 s <- colSums(Filter(is.numeric, df))
 sum_toc <- Sys.time()
-sum_time <- sum_toc - sum_tic
+sum_time <- as.double(difftime(sum_toc, sum_tic))
 
 results = list(cmd = "R-read.csv", load_time = load_time, mem = mem, sum_time = sum_time)
 json = rjson::toJSON(results)
