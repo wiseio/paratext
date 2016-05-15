cab3b485790b356b7b5dd75554a785af68407530
Damian Eads <damian@wise.io>
2016-05-14 23:57:09 -0700

    tests disk performance

---
+++ bench/test_disk.sh
@@ -0,0 +1,17 @@
+# Tests a filesystem's warm and cold performance over time. Run the directory where the device is mounted.
+# Throttled disks should result in variable results. Otherwise, the results should be closer to constant.
+
+device=$1
+log=$2
+
+disk_results = "$(sudo bash -c \"hdparm -Tt ${device} | grep Timing | sed 's/.*=//g' | sed 's/ MB\/sec//g'\")"
+disk_A = "$(echo $disk_results | cut -d' ' -f1)"
+disk_B = "$(echo $disk_results | cut -d' ' -f2)"
+
+for i in $(seq 1 1000);
+do
+  sudo bash -c "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"
+  run_experiment.py - cmd=countnl disk_state="cold" filename="floats.csv" diskA="$disk_A" diskB="$disk_B" log=$log
+  run_experiment.py - cmd=countnl disk_state="warm" filename="floats.csv" log=$log
+done
+
