6130549d1e2ba854c3e7650dc12642579b35d27e
Damian Eads <damian@wise.io>
2016-05-18 16:55:10 -0700

    Fixed typo.

--- bench/convert_benchmark_files.sh
+++ bench/convert_benchmark_files.sh
@@ -3,18 +3,18 @@
 # Convert original CSV files to different formats.
 
 echo mnist
-python convert_to_binary.py 1 mnist.csv mnist.feather mnist.hdf5 mnist.pkl mnist.npy
+python convert_to_binary.py mnist.csv 1 mnist.feather mnist.hdf5 mnist.pkl mnist.npy
 echo messy
-python convert_to_binary.py 1 messy.csv messy.feather messy.pkl messy.npy
+python convert_to_binary.py messy.csv 1 messy.feather messy.pkl messy.npy
 echo messy2
-python convert_to_binary.py 1 messy2.csv messy2.feather messy2.pkl messy2.npy
+python convert_to_binary.py messy2.csv 1 messy2.feather messy2.pkl messy2.npy
 echo mnist8m
-python convert_to_binary.py 1 mnist8m.csv mnist8m.feather mnist8m.hdf5 mnist8m.pkl mnist8m.npy
+python convert_to_binary.py mnist8m.csv 1 mnist8m.feather mnist8m.hdf5 mnist8m.pkl mnist8m.npy
 echo car
-python convert_to_binary.py 1 car.csv car.feather car.pkl car.npy
+python convert_to_binary.py car.csv 1 car.feather car.pkl car.npy
 echo floats
-python convert_to_binary.py 0 floats.csv floats.feather floats.pkl floats.npy floats.hdf5
-python convert_to_binary.py 0 floats2.csv floats2.feather floats2.pkl floats2.npy floats2.hdf5
-python convert_to_binary.py 0 floats3.csv floats3.feather floats3.pkl floats3.npy floats3.hdf5
-python convert_to_binary.py 0 floats4.csv floats4.feather floats4.pkl floats4.npy floats4.hdf5
+python convert_to_binary.py floats.csv 0 floats.feather floats.pkl floats.npy floats.hdf5
+python convert_to_binary.py floats2.csv 0 floats2.feather floats2.pkl floats2.npy floats2.hdf5
+python convert_to_binary.py floats3.csv 0 floats3.feather floats3.pkl floats3.npy floats3.hdf5
+python convert_to_binary.py floats4.csv 0 floats4.feather floats4.pkl floats4.npy floats4.hdf5
 
