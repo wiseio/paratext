380a7c9bd023eb034a7756696292eaba65b65fbb
Damian Eads <damian@wise.io>
2016-09-19 18:35:31 -0700

    added CI scripts

--- ci/linux_install.sh
+++ ci/linux_install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo apt-get update -qq
-sudo apt-get install -qq g++-4.8
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update -qq
+apt-get install -qq g++-4.8
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
