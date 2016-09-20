0807b79c5eec147403dfc1195f919594a63dc011
Damian Eads <damian@wise.io>
2016-09-19 18:41:44 -0700

    syntax error

--- ci/linux_install.sh
+++ ci/linux_install.sh
@@ -8,6 +8,7 @@ if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
 else
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
 
 echo "g++-4.8" > .cxx.choice
 
--- ci/osx_install.sh
+++ ci/osx_install.sh
@@ -6,5 +6,6 @@ if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh;
 else
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+fi
 
 echo "clang++" > .cxx.choice
